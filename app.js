6733886d84f8717dde88bd5229e7f818c1a2d5cf
Allan James Lapid <ajLapid718@gmail.com>
2019-06-17 14:23:16 -0400

    Serve static

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+const path = require("path");
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 1234; /* NEW ADDITION */
+const PORT = process.env.PORT || 1234;
 
 app.get("/students", (req, res, next) => {
   res.status(200).json([{id: 1, name: "Axel"}, {id:2, name: "Chris"}, {id: 3, name: "Michelle"}]);
 })
 
-app.listen(PORT, () => { /* NEW ADDITION */
+let locationOfPublicFolder = path.join(__dirname, "client", "build")
+app.use(express.static(locationOfPublicFolder));
+
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!!!`);
 })
